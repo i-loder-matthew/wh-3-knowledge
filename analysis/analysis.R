07ca0a5361ccd09a26c3019f77e58276e8ab4d3a
Matthew Loder <i.loder.matthew@gmail.com>
2019-11-20 12:51:50 -0500

    Update stim

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,5 +1,7 @@
 #### ANALYSIS SCRIPT DRAFT 1 ####
 
+# Creat a skeleton stim file with the items, question words and any other information potentially relevant to the analysis. 
+
 ## CLEANING SCRIPTS ##
 
 library(ggplot2)
@@ -10,6 +12,7 @@ library(gridExtra)
 library(DescTools)
 library(splines)
 library(stringr)
+library(lme4)
 
 # Function to read in data 
 read.pcibex <- function(filepath, auto.colnames=TRUE, fun.col=function(col,cols){cols[cols==col]<-paste(col,"Ibex",sep=".");return(cols)}) {
@@ -91,12 +94,51 @@ by_item_all = df.all2 %>%
   group_by(item) %>%
   summarise('in-situ' = mean(c1))
 
-plot1 = ggplot(data = percent_in_situ, mapping = aes(x = 'condition', y = 'in-situ')) +
-  geom_bar(stat = 'identity')
+by_item_participant = df.target %>%
+  group_by(item, id) %>%
+  summarise('in-situ' = mean(c1))
 
-plot1
+df.partial = df.target %>%
+  filter(condition == "partial-knowledge")
 
-## Manipulate data
+df.none = df.target %>% 
+  filter(condition == "no-knowledge")
+
+by_item_partial = df.partial %>%
+  group_by(item) %>%
+  summarise('in-situ' = mean(c1))
+
+by_item_none = df.none %>%
+  group_by(item) %>%
+  summarise('in-situ' = mean(c1))
+
+
+df.no_quoi = df.target %>%
+  filter(item == 7 | item == 12 | item == 13 | item == 6 | item == 18 | item == 19)
+
+percent_in_situ_no_quoi = df.no_quoi %>%
+  group_by(condition) %>%
+  summarise('in-situ' = mean(c1))
+
+## ANALYZE DATA 
+
+knowledgeability.model = lmer(c1 ~ condition + (1|id) + (1|item), data=df.target)
+
+knowledgeability.null = lmer(c1 ~ 1 + (1|id) + (1|item), data=df.target)
+
+summary(knowledgeability.model)
+summary(knowledgeability.null)
+
+anova(knowledgeability.null, knowledgeability.model)
+
+know.simple = lm(c1 ~ condition, data=df.target)
+
+summary(know.simple)
+
+
+
+
+
 
 
 
